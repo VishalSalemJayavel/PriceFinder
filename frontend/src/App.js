884aa0e1a24ccca614bbea72f844f77661cc0e7a
VishalSalemJayavel <sjvishal9531@gmail.com>
2023-09-04 17:13:32 -0400

    created test component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-
+import {Navbar, Header} from './components';
 
 const App = () => {
   return (
     <div className="App">
- 
-
+    <Navbar />
+    <Header />
      
     </div>
   )
