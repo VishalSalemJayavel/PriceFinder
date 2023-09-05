632bdad5572555a2427992fb22c011f460cb8467
sutharsanan007 <sutharsananravi@gmail.com>
2023-09-05 16:19:59 -0400

    updated navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import'./navbar.css'
 const navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'best seller in your area', 'fresh vegetables', 'dairy & milk', 'about'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
   )
 }
 
