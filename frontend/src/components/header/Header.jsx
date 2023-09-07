a7972ab0eeaba89569e2833d14d5fc10a15966a5
sutharsanan007 <sutharsananravi@gmail.com>
2023-09-07 15:40:17 -0400

    Added Header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
 
 import './header.css'
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="app__header">
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header_img"
+      >
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.header}
+          alt="header_img"
+          className="overlay_img"
+        />
+
+      </motion.div>
     </div>
   )
 }
--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -2,10 +2,12 @@ import cart from '../assets/cart.png';
 import logo from '../assets/logo.png';
 import sign_in from '../assets/sign_in.png';
 import wishlist from '../assets/wishlist.png';
+import header from '../assets/header.png';
 
 export default {
     cart,
     logo,
     sign_in,
     wishlist,
+    header,
 };
