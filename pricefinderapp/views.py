f6f0d261b2b438ba35c9210f6bc2a2f2962a6f16
SaiyanSai <sents@farmingdale.edu>
2023-09-07 17:32:16 -0400

    dummy response added;

--- pricefinderapp/views.py
+++ pricefinderapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
     return HttpResponse("You are not supposed to be here!")
+
+def dummyresponse(request):
+    if request.method == 'POST':
+        return JsonResponse({'foo':'bar'})
