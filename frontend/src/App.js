0dc75c81fc1c576f9aa6d0411f3086bb09462c53
sutharsanan007 <sutharsananravi@gmail.com>
2023-09-21 16:26:54 -0400

    Updated the products page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import './App.css';
-import featured from "./featured.js";
-
 
 import { Navbar, Header, Category } from './components';
 
 const App = () => {
-  const CategoryCards = featured.map((item) => {
-    return (
-      <Category
-        key={item.id}
-        item={item}
-      />
-    )
-  });
-
 
   return (
     <div data-test="test" >
       <Navbar />
       <Header />
-      {CategoryCards}
+      <Category />
     </div>
   )
 }
--- frontend/src/components/category/Category.jsx
+++ frontend/src/components/category/Category.jsx
@@ -1,22 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { images } from '../../constants'
 import './category.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const Category = (props) => {
-    console.log(props.item.coverImg)
+const Category = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('/api/categories');
+                const data = response.data;
+
+                const categoryArray = data.map((category) => ({
+                    categoryName: category.name,
+                    imgSrc: category.image,
+                }));
+
+                setCategories(categoryArray);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <div className="app__featured">
             <div className='app__featured-title'>
                 <p>Categories</p>
             </div>
-            <div className='app__featured-card'>
+            {categories.map((category, index) => (
+                <div key={index} className='app__featured-card'>
+                    <Link to="/products">
+                        <img src={category.imgSrc} alt={category.categoryName} />
+                        <p>{category.categoryName}</p>
+                    </Link>
+                </div>)
+            )}
+            {/* <div className='app__featured-card'>
                 <Link to="/products">
-                    <img src={images.tomato} className="card__image" alt={props.item.title} />
-                    <p className="card__title">{props.item.title}</p>
+                    <img src={categories.imgSrc} className="card__image" alt={images.tomato} />
+                    <p className="card__title">{categories.name}</p>
                 </Link>
-            </div>
+            </div> */}
         </div>
     )
 }
