9f08bfc928254d19a61e0b647c63ab6385bdf079
sutharsanan007 <sutharsananravi@gmail.com>
2023-10-30 18:40:07 -0400

    Finally fixed the Landing page

--- frontend/src/pages/landingPage/Landing.jsx
+++ frontend/src/pages/landingPage/Landing.jsx
@@ -1,13 +1,57 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Category, Header } from '../../components';
+import { images } from '../../constants';
 import { MainLayout } from '../../layout';
 
 const Landing = () => {
+  const [user, setUser] = useState({
+    user_type: "",
+  });
+
+  const [pageReloaded, setPageReloaded] = useState(false);
+
+  useEffect(() => {
+
+    if (!pageReloaded) {
+      const fetchData = async () => {
+        try {
+          const { data } = await axios.get('userdetails/',
+            { headers: { 'Content-Type': 'application/json' }, 'Authorization': `Bearer ${localStorage.getItem('access_token')}` },
+            { withCredentials: true });
+
+          setUser({
+            user_type: data['user_type'],
+          });
+
+        } catch (error) {
+          console.error(error); // Handle error response
+        }
+      };
+
+      fetchData(); // Fetch data from API
+      setPageReloaded(true);
+    }
+
+  }, [pageReloaded]);
+
 
   return (
     <MainLayout>
       <Header />
-      <Category />
+      <div>
+        {user.user_type === "retailer" ? (
+          <div className='app__uploadprod' style={{ display: 'flex', justifyContent: 'center', fontFamily: 'var(--font-family)' }}>
+            <Link to="/productupload">
+              <img src={images.upload} alt="dp" className='app__uploadprod-img' style={{ margin: '2rem', marginLeft: '8.5rem' }} />
+              <h1 style={{ marginLeft: '1rem' }}>Upload your Product</h1>
+              <p  >Click here to upload the details of your product</p>
+            </Link>
+          </div>
+        ) : null}
+        <Category />
+      </div>
     </MainLayout>
   );
 };
